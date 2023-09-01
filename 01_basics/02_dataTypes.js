e5e3d8d1626e9b482d6ac2a2ae25d59a60d0cc69
Mihir Jadhav <jadhavmihir143@gmail.com>
2023-09-01 14:48:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert("HELLO!") - we are using nodejs, not browser.
+// code readability should be high.
+
+
+let name = "Mihir"
+let age = 18
+let isLoggedIn = false
+
+// number -> 2 to the power 53
+// bigint 
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> value not assigned
+// symbol -> used to find uniqueness between components in react.
+
+// object
+
+console.log(typeof null) // output : object
+console.log(typeof undefined) // object : undefined
