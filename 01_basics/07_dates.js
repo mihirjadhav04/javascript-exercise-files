f19c5844b317f116317485c8a718efc1c5a70bbd
Mihir Jadhav <jadhavmihir143@gmail.com>
2023-09-01 21:32:30 +0000

    date and time.

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,31 @@
+// Dates 
+
+// Date is a object in JS.
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+
+let myCreatedDate = new Date(2023,1,23)
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
+
+// YYYY-MM-DD
+// DD-MM-YYYY
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp); // miliseconds
+// console.log(myCreatedDate.getTime());  // converts to miliseconds.
+console.log(Math.round(Date.now()/1000)); // converts to seconds.
+
+
+
+// let newDate = new Date()
+// newDate.getMonth() + 1
+// newDate.getDay()
+// newDate.getYear()
+
+
