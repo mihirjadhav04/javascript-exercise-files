73a5b994de9705ace0d88f84ce51ea45db738c9a
Mihir Jadhav <jadhavmihir143@gmail.com>
2023-09-01 15:26:59 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// comparison and == works differently.
+console.log("2" < 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// === : strict check. 
