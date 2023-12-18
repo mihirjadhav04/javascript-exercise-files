3ead1e7be058d4b6b14a6c90d68e13b0dd7925ca
Mihir Jadhav <jadhavmihir143@gmail.com>
2023-12-18 17:38:47 +0000

    scopes introduction.

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+
+
+// {} - scope 
+//global scope
+
+if (true) {
+    //block scope
+    let a = 10
+    const b = 20
+    var c = 30 // can be accessed outside the scope.
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
