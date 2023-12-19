efe025f351c9162649eaf1d2929028878283258e
Mihir Jadhav <jadhavmihir143@gmail.com>
2023-12-19 14:13:03 +0000

    control flow completed.

--- 04_controlflow/truthy_falsy.js
+++ 04_controlflow/truthy_falsy.js
@@ -17,3 +17,21 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0){
     console.log("Empty Object!");
 }
+
+
+
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+
+// val1 = 5 ?? 10
+
+val1 = null ?? 10 // instead of 10 we can use some function of db.
+
+console.log(val1);
+
+
+// Terniary Operator [condition ? true : false ]
+
