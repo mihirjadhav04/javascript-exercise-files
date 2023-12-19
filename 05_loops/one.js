e2386fc0ee99ae0eaf8a04e941083191a79ab983
Mihir Jadhav <jadhavmihir143@gmail.com>
2023-12-19 14:23:58 +0000

    for and while loop

---
+++ 05_loops/one.js
@@ -0,0 +1,8 @@
+//for loop : basic for loop that you have learnt.
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+
+// break & continue keyword.
---
+++ 05_loops/two.js
@@ -0,0 +1,9 @@
+// WHILE LOOP
+// DO WHILE LOOP
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`value of index is: ${index}`);
+    index = index + 2
+}
